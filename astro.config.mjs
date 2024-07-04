f75aa05d87f032dbb574898d3af4c10479695ab8
Luis Enrique <enriquecalero284@gmail.com>
2024-07-04 10:22:15 -0500

    Formulario diseño acabado, purgecss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,14 @@
 import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
 import react from "@astrojs/react";
-
 import sitemap from "@astrojs/sitemap";
+
+import purgecss from "astro-purgecss";
 
 // https://astro.build/config
 export default defineConfig({
-  site : 'https://www.ift.pe/',
+  site: 'https://www.ift.pe/',
   compressHTML: true,
-  build: {
-    inlineStylesheets: 'always'
-  },
   devToolbar: {
     enabled: false
   },
@@ -18,10 +16,14 @@ export default defineConfig({
     build: {
       rollupOptions: {
         output: {
-          assetFileNames: 'assets/asset.[hash][extname]',
+          assetFileNames: 'assets/asset.[hash][extname]'
         }
       }
     }
   },
-  integrations: [icon(), react(), sitemap()],
+  integrations: [icon(), react(), sitemap(), purgecss({
+    safelist: {
+      greedy: [/@/, /:/]
+    }
+  })]
 });
