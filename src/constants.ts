d1e19dd84a533aa6b45567408933fd79581e833d
Luis Enrique <enriquecalero284@gmail.com>
2024-07-26 16:20:57 -0500

    Hover Image add mas mejores en importaciones

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,3 @@
 export const PLACEHOLDER_FORMATS = 'webp';
 export const IMAGE_FORMATS = ['webp'];
+export const API_SHEETS = "https://script.google.com/macros/s/AKfycbxyrmrDyh7s9swghS79GY_BKpw5bbCeExy9I57vrSgoZIBRBrX-tT_YSCzkLA27fwQlwQ/exec";
