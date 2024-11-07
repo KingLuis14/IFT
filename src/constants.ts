397313f16eac3e17ff75ee859958f4c1a58c586f
Luis Enrique <enriquecalero284@gmail.com>
2024-11-06 23:45:18 -0500

    Form button loading + constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
-export const PLACEHOLDER_FORMATS = 'webp';
-export const IMAGE_FORMATS = ['webp'];
-export const API_SHEETS = "https://script.google.com/macros/s/AKfycbxyrmrDyh7s9swghS79GY_BKpw5bbCeExy9I57vrSgoZIBRBrX-tT_YSCzkLA27fwQlwQ/exec";
-export const URL_SITE = 'http://astro-curso-product.local/wp-json/wp/v2/cursos?acf_format=standard';
 export const IMG__DEFAULT = '/img/logo.png';
+export const NUM__WSP = '51980816746';
+export const IFT_EMAIL = "enriquecalero284@gmail.com";
+
+//informes@ift.pe
