fa48a1e989e42b6c6832bde3b397dc76cfed48c3
Luis Enrique <enriquecalero284@gmail.com>
2024-11-07 10:09:11 -0500

    Datos actualizados ift constantes

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const IMG__DEFAULT = '/img/logo.png';
 export const NUM__WSP = '51980816746';
-export const IFT_EMAIL = "enriquecalero284@gmail.com";
+export const IFT_EMAIL = "informes@ift.pe";
 
 //informes@ift.pe
